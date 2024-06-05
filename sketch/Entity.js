e5e0f86a25486d090229a27426c72fe98ce4ebd8
frick <frick@github.com>
2024-06-05 23:00:36 +0200

    Commit prior to Map change

--- sketch/Entity.js
+++ sketch/Entity.js
@@ -1,31 +1,31 @@
 class BoundingBox {
 	constructor(x, y, w, h) {
-	  this.left = x
-	  this.top = y
-	  this.right = x + w
-	  this.bottom = y + h
-	  this.width = w
-	  this.height = h
+	  	this.left = x
+	  	this.top = y
+	  	this.right = x + w
+	  	this.bottom = y + h
+	  	this.width = w
+	  	this.height = h
 	}
   
 	update(newX, newY) {
-	  this.left = newX
-	  this.top = newY
-	  this.right = newX + this.width
-	  this.bottom = newY + this.height
+		this.left = newX
+	  	this.top = newY
+	  	this.right = newX + this.width
+	  	this.bottom = newY + this.height
 	}
-  }
+}
   
-  /**
-   * Entity class
-   */
-  class Entity {
-	id = "";
-
+/**
+ * Entity class
+ */
+class Entity {
+	//super update, should be called when bounding box changes have been made
 	update() {
 	  this.bounds.update(this.xPos, this.yPos)
 	}
-  
+
+	//Collision check
 	isCollidingWith(otherEntity) {
 	  var b1 = this.bounds
 	  var b2 = otherEntity.bounds
@@ -35,19 +35,20 @@ class BoundingBox {
 		b1.top <= b2.bottom && b1.bottom >= b2.top
 	  )
 	}
-  
+	
+	//Create the bounding box for this entity.
+	//If no height is supplied, width is used.
 	constructor(xPos, yPos, width, height = width) {
 	  this.xPos = xPos
 	  this.yPos = yPos
 	  this.bounds = new BoundingBox(xPos, yPos, width, height);
-	  this.id = Math.random()*100000;
 	}
-  }
+}
   
-  /**
-   * Player class
-   */
-  class Player extends Entity {
+/**
+ * Player class
+ */
+class Player extends Entity {
 	color = { R: 200, G: 0, B: 0, A: 255 }
   
 	constructor(xPos, yPos, width, height) {
@@ -73,12 +74,12 @@ class BoundingBox {
 		this.bounds.height
 	  )
 	}
-  }
+}
   
-  /**
-   * Bottle class
-   */
-  class Bottle extends Entity {
+/**
+ * Bottle class
+ */
+class Bottle extends Entity {
 	static width = 10
 	static height = 30
 	color = { R: 0, G: 200, B: 0, A: 255 }
@@ -89,22 +90,24 @@ class BoundingBox {
 	}
   
 	update() {
-	  this.yPos += this.fallSpeed
-	  if(this.yPos > height){
-		console.warn("yes", bottles.indexOf(this));
-		bottles.splice(bottles.indexOf(this), 1);
-	  }
-	  super.update()
-	  if (this.isCollidingWith(player) && this.color.A > 0) {
-		this.color.A = 0
-		sounds.b.setVolume(0.5);
-		sounds.b.rate(1.5);
-		sounds.b.play();
-		bottles.splice(bottles.indexOf(this), 1);
-	  }
-	  else{
-		this.draw();
-	  }
+		//Gravity
+		this.yPos += this.fallSpeed
+		//Ensure still in playfield
+		if(this.yPos > height){
+			console.warn("yes", bottles.indexOf(this));
+			bottles.splice(bottles.indexOf(this), 1);
+		}
+	  	super.update()
+	  	if (this.isCollidingWith(player) && this.color.A > 0) {
+			this.color.A = 0
+			sounds.b.setVolume(0.5);
+			sounds.b.rate(1.5);
+			sounds.b.play();
+			bottles.splice(bottles.indexOf(this), 1);
+	  	}
+	  	else{
+			this.draw();
+	  	}
 	}
   
 	draw() {
@@ -118,5 +121,10 @@ class BoundingBox {
 		this.bounds.height
 	  )
 	}
-  }
+
+	updateFallSpeed(newVal){
+		this.fallSpeed = newVal;
+		return this;
+	}
+}
   
--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,8 +1,8 @@
-var player
-var bottles
-var trash
+var player;
+var bottles;
+var trash;
 
-var sounds
+var sounds;
 
 function preload() {
 	soundFormats("wav");
@@ -13,46 +13,74 @@ function preload() {
 			x.rate(1.25);
 			return x;
 		})(),
-		b: loadSound("./sfx/Electronic high shot")
-	}
+		b: loadSound("./sfx/Electronic high shot"),
+		bouw: (() => {
+			var x = loadSound("./sfx/bouw");
+			x.setVolume(0.2);
+			x.rate(1.5);
+			return x;
+		})()
+	};
 }
 
 function setup() {
-  // Phone or desktop?
-  var displayRatio = 16 / 9
-  var wH = windowHeight
-  var wW = wH * (1 / displayRatio)
-  var x = createCanvas(wW, wH, "webgl")
-  x.mouseClicked(clickHandler)
-  player = new Player(0, height - 50, 50, 50)
-  bottles = []
-  trash = []
+  // Phone or desktop? Retain a 9:16 ratio regardless
+  var displayRatio = 9 / 16;
+  var wH = windowHeight;
+  var wW = wH * displayRatio;
+  var x = createCanvas(wW, wH, "webgl");
+  x.mouseClicked(clickHandler);
+  player = new Player(0, height - 50, 50, 50);
+  bottles = [];
+  trash = [];
 }
 
+let ticker = 0;
+let tickTrigger = 20;
+
 function draw() {
-  fill("red");
-  rect(0, 0, 200, 200);
-  translate(-width / 2, -height / 2)
-  background(220)
-  fill(255, 0, 0)
-  player.update()
-  player.draw()
-  var i = 0;
-  while(bottles.length > 0 && bottles[i]){
-	bottles[i].update();
-	i++;
-  }
+  	fill("red");
+  	rect(0, 0, 200, 200);
+  	translate(-width / 2, -height / 2)
+  	background(0)
+  	fill(255, 0, 0)
+  	player.update()
+  	player.draw()
+  	var i = 0;
+  	while(bottles.length > 0 && bottles[i]){
+		if (bottles[i].update()){
+			i++;
+		}
+		else {
+			
+		}
+  	}
+
+	if(ticker++ >= tickTrigger){
+		ticker = 0;
+		spawnBottle();
+	}
 }
 
 function clickHandler() {
-  bottles.push(new Bottle(mouseX, mouseY))
-  sounds.a.play()
-  console.log("New Bottle placed:", mouseX, mouseY)
+	//Debug function
+  bottles.push(new Bottle(mouseX, mouseY));
+  sounds.bouw.play();
+  console.log("New Bottle placed:", mouseX, mouseY);
 }
 
-class Point {
-  constructor(x, y) {
-    this.X = x
-    this.Y = y
-  }
+function spawnBottle(){
+	//Determine x-coordinate
+	let margin = 5;
+	let maxLeft = width - (Bottle.width + margin);
+	let xPos = Math.random() * maxLeft;
+	let yPos = 0;
+
+	//Determine the fall speed
+	let baseSpeed = 5;
+	let speedSpread = 2.5
+	bottles.push(
+		new Bottle(xPos, yPos)
+		.updateFallSpeed(baseSpeed + Math.random() * speedSpread));
+	sounds.bouw.play();
 }
