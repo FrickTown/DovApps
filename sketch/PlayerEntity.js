2adc3e9e54df587404b90a35ffedb3c32f7160b5
frick <frick@github.com>
2024-06-10 14:20:18 +0200

    Codebase refactoring

---
+++ sketch/PlayerEntity.js
@@ -0,0 +1,39 @@
+/**
+ * PlayerEntity class
+ * Extends the Entity with:
+ * @member Color: an object with RGBA values
+ */
+class PlayerEntity extends Entity {
+
+	Color = { R: 200, G: 0, B: 0, A: 255 };
+	SizeRatio = 1.0;
+
+	constructor(xPos, yPos, width) {
+		super(xPos, yPos, width, width);
+		this.Texture = textures.korg;
+		this.SizeRatio = this.Texture.height / this.Texture.width;
+		this.Bounds.height = this.Bounds.width * this.SizeRatio;
+	}
+
+	update() {
+		var nextX = mouseX;
+		if (nextX <= 0) nextX = 0;
+		if (nextX + this.Bounds.width >= width) nextX = width - this.Bounds.width;
+		this.X = nextX;
+		this.Y = height - this.Bounds.height;
+		//Ancestor's 
+		super.update();
+	}
+
+	draw() {
+		fill(this.Color.R, this.Color.G, this.Color.B, this.Color.A);
+		stroke("black");
+		texture(textures.korg);
+		rect(
+			this.Bounds.left,
+			this.Bounds.top,
+			this.Bounds.width,
+			this.Bounds.height
+		);
+	}
+}
